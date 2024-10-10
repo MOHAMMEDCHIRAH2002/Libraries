49131ed201defb8e9e30877c68284e694e110e93
Medchirah <mohammed.chirah@gmail.com>
2024-10-10 12:56:41 +0100

    first commit

---
+++ src/test/java/com/Ecommerce/Libraries/LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.Libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
